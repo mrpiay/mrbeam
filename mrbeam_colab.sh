b8bd247c223f39f2c828459ecbb4bb5ee46058fc
MrPiay <30749947+mrpiay@users.noreply.github.com>
2024-05-06 00:30:51 +0200

    Update mrbeam_colab.sh

--- mrbeam_colab.sh
+++ mrbeam_colab.sh
@@ -3,10 +3,10 @@ unzip master.zip -d .
 wget https://github.com/achael/eht-imaging/archive/refs/tags/v1.2.7.zip
 unzip v1.2.7.zip -d .
 pip install pygmo
-pip install ./ehtplot-master
-pip install ./eht-imaging-1.2.7
-pip install ./mrbeam/mr_beam/itreg
-pip install ./mrbeam/mr_beam/libwise-0.4.7-light
-pip install ./mrbeam/mr_beam/MSI
-pip install ./mrbeam/mr_beam/imagingbase
-pip install ./mrbeam/mr_beam/ga
+pip install ehtplot-master
+pip install eht-imaging-1.2.7
+pip install ./mr_beam/itreg
+pip install ./mr_beam/libwise-0.4.7-light
+pip install ./mr_beam/MSI
+pip install ./mr_beam/imagingbase
+pip install ./mr_beam/ga
